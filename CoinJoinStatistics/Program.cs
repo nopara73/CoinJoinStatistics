7ccf4ff17e4733d2578b811cb02468fa1e5b1f06
nopara73 <adam.ficsor73@gmail.com>
2019-01-01 18:43:41 +0100

    Add project files.

---
+++ CoinJoinStatistics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoinJoinStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
